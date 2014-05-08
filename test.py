f5afdc74716a3308dd936ca8f2b9989b53c23920
wangyuan <wang.yuan@youku.com>
2014-05-08 16:06:52 +0800

    调整架构，完成connect

--- test.py
+++ test.py
@@ -1,1 +1,40 @@
 #coding:utf-8
+from tornado.testing import AsyncTestCase, gen_test
+from tornado_memcache import Client, ConnectionTimeoutError
+import tornado.ioloop
+
+class BaseTestCase(AsyncTestCase):
+    'Base Test Case'
+
+    def get_new_ioloop(self):
+        
+        return tornado.ioloop.IOLoop.instance()
+
+class ClientTestCase(BaseTestCase):
+    'Client Test Case'
+
+    @gen_test
+    def test_connect(self):
+        '测试正常连接'
+        
+        host = '127.0.0.1:11211'
+        client = Client([host])
+        yield client.connect(host)
+        self.assertIn(host, client.connection_map)
+ 
+    @gen_test
+    def test_connect_when_no_socket(self):
+        '测试连接不存在的服务'
+        
+        host = '127.0.0.1:45445'
+        client = Client([host], connection_timeout=0.1)
+        try:
+            yield client.connect(host)
+            self.assertTrue(False)
+        except ConnectionTimeoutError:
+            self.assertTrue(True)
+
+if __name__ == '__main__':
+
+    import unittest
+    unittest.main()
--- tornado_memcache.py
+++ tornado_memcache.py
@@ -1,61 +1,86 @@
 #coding:utf-8
 import tornado.iostream
-import tonrado.gen
-from binascii import crc32   # zlib version is not cross-platform
+import tornado.gen
+from binascii import crc32
+import tornado.ioloop
+from tornado import stack_context
+import time
+import socket
+from functools import partial
+import logging
 
-__author__ == 'Wang Yuan <yuanwang.bupt@gmail.com>'
-__version__ == '1.0'
+__author__ = 'Wang Yuan <yuanwang.bupt@gmail.com>'
+__version__ = '1.0'
 
 def cmemcache_hash(key):
     
     return((((crc32(key) & 0xffffffff) >> 16) & 0x7fff) or 1)
 
-class ConnectionError(Exception):
+class TimeoutError(Exception):
     pass
 
-class SocketIOError(Exception):
-    pass
-
-class ConnectionTimeoutError(ConnectionError):
-    pass
-
-class ReadTimeoutError(SocketIOError):
-    pass
-
-class WriteTimeoutError(SocketIOError):
+class ConnectionTimeoutError(TimeoutError):
     pass
 
 class Client:
-    'clients'
+    'client'
     
-    def __init__(self, hosts, connection_timeout=3):
+    def __init__(self, hosts, logger=logging, io_loop=None, connection_timeout=1):
         
         self.hosts = hosts
+        self.dead_hosts = hosts
+        self.io_loop = io_loop or tornado.ioloop.IOLoop.instance()
+        self.connection_map = {}
+        self.logger = logger
         self.connection_timeout = connection_timeout
-
-    @tornado.gen.coroutine
-    def get(self, timeout=1):
-        'get value by key'
-
+    
+    def get(self, key):
+        
+        pass
+    
+    def set(self, key, value, expire):
+        
+        pass
+    
+    def send_cmd(self, cmd, host):
+        
+        pass
+    
+    def parse_response(self, response):
+        
         pass
 
     @tornado.gen.coroutine
-    def set(self, key, value, expire, timeout=1):
-        'set value for key'
+    def connect(self, host):
+        'connect to host'
 
-        pass
-
-    @tornado.gen.coroutine
-    def send_cmd(self, cmd, timeout=1):
-        'send command'
-
-        pass
-
-    @tornado.gen.coroutine
-    def read_result(self, timeout=1):
-        'read result'
-
-        pass
-
-if __name__ == '__main__':
-    pass
+        _timeout_handle = self.add_timeout(self.connection_timeout, error=ConnectionTimeoutError)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        stream = tornado.iostream.IOStream(s)
+        _host, _port = host.split(':', 1)
+        yield tornado.gen.Task(stream.connect, (_host, int(_port)))
+        self.remove_timeout(_timeout_handle)
+        self.connection_map[host] = stream
+        raise tornado.gen.Return(1)
+    
+    def close(self):
+        'close all streams'
+        
+        for stream in self.connection_map.values():
+            stream.close()
+    
+    def _on_timeout(self, error=TimeoutError):
+        'timeout callback'
+        
+        raise error()
+    
+    def add_timeout(self, seconds, error=TimeoutError):
+        'add new timeout handle'
+        
+        return self.io_loop.add_timeout(time.time() + seconds, partial(self._on_timeout, error=error))
+    
+    def remove_timeout(self, timeout_handle):
+        'remote one timeout handle'
+        
+        if isinstance(timeout_handle, tornado.ioloop._Timeout):
+            self.io_loop.remove_timeout(timeout_handle)
